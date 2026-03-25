fed1a4213227867c78d6aec64eee5633b4c9ec49
mbxta9 <235346231+mbxta9@users.noreply.github.com>
2026-03-25 11:51:35 +0000

    Updating Missingness script

--- Scripts/8b_Missingness.r
+++ Scripts/8b_Missingness.r
@@ -3,15 +3,11 @@
 ########################################
 
 # Load files
-imiss <- read.table("dog/dog_missing.imiss", header=TRUE)
-lmiss <- read.table("dog/dog_missing.lmiss", header=TRUE)
+imiss <- read.table("dog/imputed/dog_missing.imiss", header=TRUE)
+lmiss <- read.table("dog/imputed/dog_missing.lmiss", header=TRUE)
 
 fam_file <- read.table("dog/dog_raw.fam", header=FALSE)
 
 # Plot histograms of missingness
 hist(imiss$F_MISS, breaks=20, main = "Sample missingness", xlab = "Proportion of missing genotypes", ylab = "Frequency")
 hist(lmiss$F_MISS, breaks=20, main = "SNP missingness", xlab = "Proportion of missing genotypes", ylab = "Frequency")
-
-length(imiss$F_MISS)
-
-head(fam_file)
